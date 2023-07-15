4b4b41e1ea9f7b729241e6b9b522565df3b5226d
Junseong Park <junseongpark.dev@gmail.com>
2023-07-15 23:21:57 +0900

    style

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Junseong Park';
-export const SITE_DESCRIPTION = 'Introduce who I am and record my experiences.';
+export const SITE_TITLE = "Junseong Park";
+export const SITE_DESCRIPTION =
+  "This website introduces who I am and shares my experiences.";
