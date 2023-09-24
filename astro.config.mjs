7cf89833b219ffdea80d75de264f7b61036bd20c
JS Park <junseongpark.dev@gmail.com>
2023-09-25 08:10:57 +0900

    feat: vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/static";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+      speedInsights: { enabled: true },
+    },
+  }),
 });
