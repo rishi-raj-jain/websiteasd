270d82fa80d0e75b36237c1aa61b331109fc656e
JS Park <junseongpark.dev@gmail.com>
2023-09-25 22:10:11 +0900

    remove react, analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "static",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-      speedInsights: { enabled: true },
-    },
-  }),
+  adapter: vercel(),
 });
