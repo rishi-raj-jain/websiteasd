27ff71e803541c9692dff2e973203148df54fe23
JS Park <junseongpark.dev@gmail.com>
2023-09-26 19:27:08 +0900

    refactor: update layout

---
+++ src/classList.ts
@@ -0,0 +1,1 @@
+export const linkHover = "transition-opacity hover:opacity-50";
---
+++ src/data.ts
@@ -0,0 +1,23 @@
+export const navMenu = [
+  {
+    href: "/",
+    title: "Home",
+  },
+  {
+    href: "/links",
+    title: "Links",
+  },
+];
+
+export const links = [
+  {
+    href: "https://github.com/jsparkdev",
+    title: "Github",
+    description: "My Github",
+  },
+  {
+    href: "https://x.com/jsparkdev",
+    title: "Twitter",
+    description: "My Twitter",
+  },
+];
