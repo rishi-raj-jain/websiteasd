77786b3f6ada491dfcf053642356e36dee8e9c66
JsPark <junseongpark.dev@gmail.com>
2023-09-27 10:42:14 +0900

    refactor(path): update

--- src/script/repository.ts
+++ src/script/repository.ts
@@ -1,7 +1,6 @@
-import type { Project } from "../types";
+import type { Project } from "@/types";
 
 const response = await fetch("https://api.github.com/users/jsparkdev/repos");
-
 const json = await response.json();
 
 const myProjects = json.filter((repo: Project) => repo.fork);
