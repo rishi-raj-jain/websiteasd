1983d1ac07123429a22d92428d42ac4e1ceca8e9
JS Park <junseongpark.dev@gmail.com>
2023-10-01 20:57:09 +0900

    refactor(introduce): update

--- src/data.ts
+++ src/data.ts
@@ -1,27 +1,55 @@
 export const navMenu = [
   {
-    href: "/",
-    title: "Home",
+    to: "/",
+    name: "Home",
   },
   {
-    href: "/links",
-    title: "Links",
+    to: "/links",
+    name: "Links",
   },
   {
-    href: "/repo",
-    title: "Repositories",
+    to: "/repo",
+    name: "Repositories",
   },
 ];
 
-export const links = [
+export const socialLinks = [
   {
-    href: "https://github.com/jsparkdev",
-    title: "Github",
+    to: "https://github.com/jsparkdev",
+    name: "Github",
     description: "My Github",
   },
   {
-    href: "https://x.com/jsparkdev",
-    title: "Twitter",
+    to: "https://x.com/jsparkdev",
+    name: "Twitter",
     description: "My Twitter",
+  },
+];
+
+export const skills = [
+  {
+    name: "TypeScript",
+    url: "https://typescriptlang.org",
+    color: "#3768f0",
+  },
+  {
+    name: "React",
+    url: "https://react.dev",
+    color: "#4bdbff",
+  },
+  {
+    name: "Remix",
+    url: "https://remix.run",
+    color: "#ff3bff",
+  },
+  {
+    name: "Next.js",
+    url: "https://nextjs.org",
+    color: "#3eff48",
+  },
+  {
+    name: "Bun",
+    url: "https://bun.sh",
+    color: "#ecff3e",
   },
 ];
