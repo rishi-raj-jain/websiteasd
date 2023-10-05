4ba7e7d47e571a473fff0f741da72402cbeb14c1
JS Park <junseongpark.dev@gmail.com>
2023-10-05 23:46:52 +0900

    style: add fade-in animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,5 +6,20 @@ module.exports = {
       white: "#fff",
       accent: "#4adcf0",
     },
+    extend: {
+      animation: {
+        "fade-in": "fadeIn 1s ease-in-out forwards",
+        "fade-in-1": "fadeIn 1s 0.25s ease-in-out forwards",
+        "fade-in-2": "fadeIn 1s 0.5s ease-in-out forwards",
+        "fade-in-3": "fadeIn 1s 0.75s ease-in-out forwards",
+        "fade-in-4": "fadeIn 1s 1s ease-in-out forwards",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0", transform: "translateY(-0.75rem)" },
+          "100%": { opacity: "1" },
+        },
+      },
+    },
   },
 };
