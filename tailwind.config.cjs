6b821f631f9f99b5159bbd30cf8c27d729da2426
JsPark <junseongpark.dev@gmail.com>
2023-10-05 13:27:48 +0900

    chore(page): update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
+  theme: {
+    colors: {
+      white: "#fff",
+      accent: "#4adcf0",
+    },
+  },
 };
