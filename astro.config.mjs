48ade68654f35c859539bab2a17239604e15325d
Junseong Park <junseongpark.dev@gmail.com>
2024-01-03 21:13:06 +0900

    feat: add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), partytown()],
   output: "hybrid",
   adapter: vercel({
     webAnalytics: {
