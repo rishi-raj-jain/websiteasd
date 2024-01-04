9b0eebd2abebd7bd57631904cf6a93f8d4cb14f2
Junseong Park <junseongpark.dev@gmail.com>
2024-01-04 13:56:58 +0900

    fix: error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  output: "hybrid",
-  adapter: vercel({}),
 });
