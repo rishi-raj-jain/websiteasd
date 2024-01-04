5d4534098a2f142014bd569c05d43f02460624d5
Junseong Park <junseongpark.dev@gmail.com>
2024-01-04 13:55:28 +0900

    fix: remove analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import mdx from "@astrojs/mdx";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), partytown()],
+  integrations: [tailwind(), mdx()],
   output: "hybrid",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    speedInsights: {
-      enabled: true,
-    },
-  }),
+  adapter: vercel({}),
 });
