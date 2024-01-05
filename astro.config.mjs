e0fa0a2a8d594bb5ea3985324e518e4a0f90f868
Junseong Park <junseongpark.dev@gmail.com>
2024-01-05 19:54:09 +0900

    feat: add starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,29 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import { astroExpressiveCode } from "@astrojs/starlight/expressive-code";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [
+    tailwind(),
+    astroExpressiveCode(),
+    mdx(),
+    starlight({
+      title: "Jspark.dev",
+      description: "웹 개발과 관련된 게시물 모음입니다.",
+      lastUpdated: true,
+      locales: {
+        root: {
+          label: "한국어",
+          lang: "ko-KR",
+        },
+      },
+      social: {
+        github: "https://github.com/jsparkdev",
+        "x.com": "https://x.com/jsparkdev",
+      },
+    }),
+  ],
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,6 @@
-import { z, defineCollection } from "astro:content";
-
-const postCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    tag: z.string(),
-    pubDate: z.date().default(() => new Date()),
-  }),
-});
+import { defineCollection } from "astro:content";
+import { docsSchema } from "@astrojs/starlight/schema";
 
 export const collections = {
-  posts: postCollection,
+  docs: defineCollection({ schema: docsSchema() }),
 };
